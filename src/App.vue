cb67f760e8c8064e6fad1135259bccc4d0cbce76
faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2023-12-09 15:06:21 +0700

    feat(todo):buat dan hapus todo

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <h1>Todo List</h1>
+
+    <form @submit.prevent="addTodo">
+      <label for="todoInput">New Todo:</label>
+      <input v-model="newTodo" type="text" id="todoInput" required>
+      <button type="submit">Add Todo</button>
+    </form>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push(this.newTodo);
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+form {
+  margin-bottom: 20px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px 0;
+}
+</style>
+
+<style scoped>
+
+</style>
+
